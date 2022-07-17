ab6500fa2d55a0606e764629a60c615c3069c0e7
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-07-18 00:27:23 +0100

    fixed unique key error and starting to implement the sort feature

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -16,7 +16,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genres });
   }
 
@@ -39,6 +39,10 @@ class Movies extends Component {
 
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handleSort = (path) => {
+    console.log(path);
   };
   render() {
     // refactoring
@@ -82,6 +86,7 @@ class Movies extends Component {
             movies={this.state.movies}
             onDelete={this.handleDelete}
             onLike={this.handleLike}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
