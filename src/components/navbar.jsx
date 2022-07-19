e0dc4b4f4695fcc35a02286cd81f854cb985c678
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-07-20 00:50:47 +0100

    class to className

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
           Vidly
         </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarTogglerDemo02"
@@ -17,7 +17,7 @@ const NavBar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
