358dc4fc831892ef8e40a4db6219138c759ee2c5
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-07-23 18:55:23 +0100

    Validate input on change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -33,9 +33,22 @@ class LoginForm extends Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
+  };
+
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
   };
   render() {
     const { username, password } = this.state.account;
