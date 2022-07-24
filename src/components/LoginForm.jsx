175f30cf5458b8ac048034d187b6451f2e5d62ca
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-07-24 15:42:30 +0100

    form component reusable

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Input from "./input";
 import Joi from "joi-browser";
-
-class LoginForm extends Component {
+import Form from "./common/form";
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
@@ -12,45 +12,12 @@ class LoginForm extends Component {
     username: Joi.string().required().label("Username"),
     password: Joi.string().required().label("Password"),
   };
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
 
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     console.log("Submitted");
   };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
   render() {
-    const { username, password } = this.state.account;
+    const { username, password } = this.state.data;
     const { errors } = this.state;
     return (
       <div>
