8cb436909e72923e81849853858e5daa210af671
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-07-24 17:09:51 +0100

    fixed state of registerform

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,19 +3,19 @@ import Joi from "joi-browser";
 import Form from "./common/form";
 class RegisterForm extends Form {
   state = {
-    data: {},
+    data: { username: "", password: "", name: "" },
     errors: {},
   };
 
   schema = {
     username: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password"),
+    password: Joi.string().required().min(5).label("Password"),
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit() {
+  doSubmit = () => {
     console.log("Registered");
-  }
+  };
   render() {
     return (
       <div>
